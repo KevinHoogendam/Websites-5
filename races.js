173ee9a127eb7c73bf109555eb4433db2f2c25bb
Roel1l <fallroel@live.nl>
2017-03-23 15:52:22 +0100

    fdssdf

--- races.js
+++ races.js
@@ -4,6 +4,11 @@ var router = express.Router();
 router.get('/', function(req, res){
 	res.send('GET called on races');
 });
+
+router.get('/:id', function(req, res){
+	res.send('The id you specified is ' + req.params.id);
+});
+
 router.post('/', function(req, res){
 	res.send('POST called on races');
 });
