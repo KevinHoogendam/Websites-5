8b863666fd875b578316dca4f14f1d6a29525b4e
Roel1l <fallroel@live.nl>
2017-03-24 14:58:02 +0100

    parsers added

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var multer = require('multer');
+var upload = multer();
+
 var app = express();
 
-var races = require('./races.js'); 
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(upload.array());
 
-app.get('/', function(req, res){
-    res.send("Welcome to our API");
-});
+//Require the Router we defined in movies.js
+var races = require('./races.js');
 
 app.use('/races', races); 
 
