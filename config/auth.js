25244a584e9459ea722fdd54fe1c64d62b828295
Kevin Hoogendam <khoogendam6@gmail.com>
2017-03-31 15:00:01 +0200

    callback

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
 	'googleAuth' : {
         'clientID'      : '1053314850498-1jcthrah64q8v5nhq1ma6gihuj59nb0p.apps.googleusercontent.com',
         'clientSecret'  : 'pbdOe74oJEaaPhlbbcGkVejL',
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+        'callbackURL'   : '/auth/google/callback'
     }
 };
