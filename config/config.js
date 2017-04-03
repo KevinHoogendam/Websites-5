5008689c828106cf761a8920c8dc512437663b13
Roel1l <fallroel@live.nl>
2017-04-03 14:27:26 +0200

    waypoints addendsf

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 module.exports = {
 	db: {
-		dev: 'mongodb://admin:admin@ds135830.mlab.com:35830/restrace',
+		//dev: 'mongodb://admin:admin@ds135830.mlab.com:35830/restrace',
+		dev: 'mongodb://localhost:27017/',
 		test: 'mongodb://localhost:27017/'
 	}, 
 };
